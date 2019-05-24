2edcd187587ee3969d59944db3a6d883ff81f68d
acieshk <acieshk@gmail.com>
2019-05-24 01:56:30 -0700

    set http cache control

--- server.js
+++ server.js
@@ -4,9 +4,16 @@ const path = require('path');
 
 const app = express();
 var compression = require('compression')
+var serveStatic = require('serve-static')
 // Serve only the static files form the dist directory
 app.use(compression())
-app.use(express.static(__dirname + '/dist/fo76-legendary-scrip-calculator'));
+app.use(
+  serveStatic(path.join(__dirname + '/dist/fo76-legendary-scrip-calculator')
+  ,{
+    maxAge: '1d',
+    setHeaders: setCustomCacheControl
+  }
+));
 
 app.get('/*', function(req,res) {
 
@@ -15,3 +22,10 @@ res.sendFile(path.join(__dirname+'/dist/fo76-legendary-scrip-calculator/index.ht
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
+function setCustomCacheControl (res, path) {
+  if (serveStatic.mime.lookup(path) === 'text/html') {
+    // Custom Cache-Control for HTML files
+    res.setHeader('Cache-Control', 'public, max-age=0')
+  }
+}
